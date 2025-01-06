7f624e6f1a5e5b16b70678c806b00f12f4eb9e9a
Jasec <jasec_7@hotmail.com>
2025-01-05 21:13:52 -0800

    comple looping lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const name = ["Guadalupe","Ollie", "Aki"];
+
+function writeCards(name) {
+    const messages = [];
+    for (let i = 0; i < name.length; i++) {
+         messages.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+       
+    }
+    return messages;
+}
+
+
+let number = 10;
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
